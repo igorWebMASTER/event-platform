b181c2fcf697b0063c4253c247d34b866eec5367
igor.sobral <igor.ribeiro@pagtel.com.br>
2022-06-21 23:20:10 -0300

    feat: add sidebar and lessons components

---
+++ src/components/Lesson/index.tsx
@@ -0,0 +1,44 @@
+import { CheckCircle, Lock } from 'phosphor-react'
+import React from 'react'
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date,
+    type: 'live' | 'class'
+}
+
+export function Lesson({ title, type, slug, availableAt }: LessonProps) {
+    const isLessonAvailable = isPast(availableAt)
+    const availableDateFormatted = format(availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+        locale: ptBR
+    })
+    return (
+        <a href='#'>
+            <span className='text-gray-300'>
+                {availableDateFormatted}
+            </span>
+            <div className='rounded border border-gray-500 p-4 mt-2'>
+                <header className='flex items-center justify-between'>
+                    {isLessonAvailable ? (
+                        <span className='text-sm text-blue-500 flex items-center gap-2'>
+                            <CheckCircle size={25} />
+                            Conteúdo liberado
+                        </span>
+                    ) : (
+                        <span className='text-sm text-orange-500 flex items-center gap-2'>
+                            <Lock size={25} />
+                            Em breve
+                        </span>)}
+                    <span className='text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold'>
+                        {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+                    </span>
+                </header>
+                <strong className='text-gray-200 mt-5 block'>
+                    {title}
+                </strong>
+            </div>
+        </a>
+    )
+}
